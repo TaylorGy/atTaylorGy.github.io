83e925ffe2e555308ad135103d58e140c909bed4
Taylor Gao <taylorgby@gmail.com>
2023-07-27 20:27:23 -0400

    change title format

--- article/sg0/static/script/generate_menu.py
+++ article/sg0/static/script/generate_menu.py
@@ -8,7 +8,7 @@ with open("menu.html", 'w', encoding='utf8') as menu:
     menu.write("<li> <a href=\"./doc/0000\"> 序章 </a> </li>\n")
     for file in list_file:
         with open(os.path.join(root, file), 'r', encoding='utf8') as f:
-            title = f.readline()[3:-1]
+            title = f.readline()[2:-1]
             # <li> <a href="./doc/0000"> Prologue </a> </li>
             li = "<li> <a href=\"./doc/" + file.split('.')[0] + "\"> " + title + " </a> </li>\n"
             menu.write(li)
