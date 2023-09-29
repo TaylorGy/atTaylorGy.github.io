acf06f642a5e082646ba1bb03f512f5a82bad44f
Taylor Gao <taylorgby@gmail.com>
2023-09-28 23:53:40 -0400

    update menu

--- article/sg0/static/script/generate_menu.py
+++ article/sg0/static/script/generate_menu.py
@@ -5,10 +5,10 @@ root = "../../doc/"
 list_file = os.listdir(root)[1:]
 
 with open("menu.html", 'w', encoding='utf8') as menu:
-    menu.write("<li> <a href=\"./doc/0000\"> 序章 </a> </li>\n")
+    menu.write("<li> <a href=\"./doc/0000\">序章</a> </li>\n")
     for file in list_file:
         with open(os.path.join(root, file), 'r', encoding='utf8') as f:
             title = f.readline()[2:-1]
             # <li> <a href="./doc/0000"> Prologue </a> </li>
-            li = "<li> <a href=\"./doc/" + file.split('.')[0] + "\"> " + title + " </a> </li>\n"
+            li = "<li> <a href=\"./doc/" + file.split('.')[0] + "\">" + title + "</a> </li>\n"
             menu.write(li)
