f455f5bf7ba2ee7f98796b3fa5c32154a9545251
Taylor Gao <taylorgby@gmail.com>
2023-11-15 00:01:09 -0500

    update homepage mobile

--- article/sg0/static/js/main.js
+++ article/sg0/static/js/main.js
@@ -5,22 +5,23 @@ $(document).ready(function () {
     var shownItems;
     const itemWidth = 240;
     const itemHeight = 50;
-    const menuHeight = $('.menu').height();
     var menuWidth = $('.menu').width();
+    var menuHeight = $('.menu').height();
     var totalPages = 1;
 
+    // 载入目录
     $('.menu').load("./static/script/menu.html", function () {
         menuItems = $('.menu li');
         shownItems = menuItems;
         generateMenuPages(menuItems, shownItems);
     });
 
-    // 根据所选路线切换目录项目
+    // 根据所选路线筛选目录条目
     $('.clearlist li').on('click', function () {
         const clickedId = $(this).attr('id');
-        if (!$(this).find('.mask1').hasClass('active')) {
-            $('.clearlist li').find('.mask1, .mask2, span').removeClass('active');
-            $(this).find('.mask1, .mask2, span').addClass('active');
+        if (!$(this).hasClass('active')) {
+            $('.clearlist li').removeClass('active');
+            $(this).addClass('active');
             if (clickedId === 'btn-all') {
                 shownItems = menuItems;
             } else if (clickedId === 'btn-kyoju') {
@@ -45,82 +46,28 @@ $(document).ready(function () {
         if ($(window).width() > demarcation) { // 只在宽屏模式生效
             if (Math.floor($('.menu').width() / itemWidth) !== Math.floor(menuWidth / itemWidth)) {
                 generateMenuPages(menuItems, shownItems);
-
             }
         }
     });
 
-    // 鼠标滚轮翻页
-    $('.menu').on('mousewheel DOMMouseScroll', function (event) {
-        if ($(window).width() > demarcation) { // 只在宽屏模式生效
-            if (totalPages > 1) {
-                const currentPage = $('.page-index li.active').attr('id');
-                let targetPage = 0;
-                if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-                    // 鼠标滚轮向上滚动，向左翻页
-                    targetPage = currentPage - 1;
-                } else {
-                    // 鼠标滚轮向下滚动，向右翻页
-                    targetPage = +currentPage + 1;
-                }
-                // console.log(targetPage);
-
-                if (1 <= targetPage <= totalPages) {
-                    $(`.page-index #${targetPage}`).click();
-                }
-            }
-        }
-    });
-
-
-    { // 触屏左右滑动翻页 
-        let touchElement = $('.menu');
-        let startX;
-        let targetPage = 0;
-        touchElement.on('touchstart', function (e) {
-            startX = e.originalEvent.touches[0].clientX;
-        });
-
-        touchElement.on('touchmove', function (e) {
-            if ($(window).width() > demarcation) { // 只在宽屏模式生效
-
-                e.preventDefault(); // 防止默认的滚动行为
-                let deltaX = e.originalEvent.touches[0].clientX - startX;
-
-                if (totalPages > 1) {
-                    const currentPage = $('.page-index li.active').attr('id');
-                    if (deltaX > 50) {
-                        // 向右滑动，向左翻页
-                        targetPage = currentPage - 1;
-                    } else if (deltaX < -50) {
-                        // 向左滑动，向右翻页
-                        targetPage = +currentPage + 1;
-                    }
-                }
-            }
-        });
-
-        touchElement.on('touchend', function (e) {
-            if ($(window).width() > demarcation) { // 只在宽屏模式生效
-
-                // 在这里可以进行触摸结束时的处理
-                if (1 <= targetPage <= totalPages) {
-                    $(`.page-index #${targetPage}`).click();
-                }
-            }
-        });
-    }
-
-    // 根据屏幕宽度和元素大小，动态计算目录的页数，并生成翻页按钮
+    // 目录生成函数
     function generateMenuPages(mItems, sItems) {
-        menuWidth = $('.menu').width();
+        // 根据所选路线显示目录条目
         mItems.hide();
         sItems.fadeIn(animeDuration);
 
-        if (menuWidth > demarcation) {
-            const itemsPerPage = Math.floor(menuHeight / itemHeight) * Math.floor(menuWidth / itemWidth);
+        if ($(window).width() > demarcation) { // 只在宽屏模式生效
+            // 根据屏幕宽高和元素大小，计算目录总页数
+            menuWidth = $('.menu').width();
+            const itemsPerRow = Math.floor(menuWidth / itemWidth);
+            const itemsPerColumn = Math.floor(menuHeight / itemHeight);
+            const itemsPerPage = itemsPerColumn * itemsPerRow;
             totalPages = Math.ceil(sItems.length / itemsPerPage);
+            // 去掉 .menu 多余的高度（目录高度应为条目高度的整数倍）
+            menuHeight = itemsPerColumn * itemHeight;
+            $('.menu').css('height', menuHeight);
 
+            // 为目录中每个条目添加所在页码
             mItems.each(function () {
                 $(this).attr('data-page', '0');
             });
@@ -129,6 +76,7 @@ $(document).ready(function () {
                 $(this).attr('data-page', pageValue);
             });
 
+            // 根据总页数生成翻页按钮，并绑定翻页方法
             $('.page-index').empty();
             for (let page = 1; page <= totalPages; page++) {
                 const button = $(`<li id=${page}></li>`);
@@ -139,7 +87,7 @@ $(document).ready(function () {
                     // mItems.hide();
                     // mItems.filter(`[data-page="${page}"]`).fadeIn(animeDuration);
                     const targetPage = $(this).attr('id');
-                    const targetDistance = (targetPage - 1) * menuWidth;
+                    const targetDistance = (targetPage - 1) * itemsPerRow * itemWidth;
                     $('.menu').animate({ scrollLeft: targetDistance }, animeDuration);
                 });
                 $('.page-index').append(button);
@@ -147,5 +95,76 @@ $(document).ready(function () {
             $('.page-index li:first').click();
         }
     }
-});
 
+    // 鼠标滚轮左右翻页
+    $('.menu').on('mousewheel DOMMouseScroll', function (event) {
+        if ($(window).width() > demarcation && totalPages > 1) { // 只在宽屏模式生效
+            const currentPage = +$('.page-index li.active').attr('id');
+            let targetPage = currentPage;
+            if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+                if (currentPage > 1) {
+                    // 鼠标滚轮向上滚动，向左翻页
+                    targetPage = currentPage - 1;
+                }
+            } else {
+                if (currentPage < totalPages) {
+                    // 鼠标滚轮向下滚动，向右翻页
+                    targetPage = currentPage + 1;
+                }
+            }
+
+            if (targetPage != currentPage) {
+                $(`.page-index #${targetPage}`).click();
+            }
+        }
+    });
+
+    { // 触屏滑动左右翻页
+        let touchElement = $('.menu');
+        let startX;
+        let currentPage = 1;
+        let targetPage = 0;
+
+        // 触摸开始时的处理
+        touchElement.on('touchstart', function (event) {
+            if ($(window).width() > demarcation && totalPages > 1) { // 只在宽屏模式生效
+                startX = event.originalEvent.touches[0].clientX;
+                currentPage = +$('.page-index li.active').attr('id');
+                targetPage = currentPage;
+            }
+        });
+
+        // 触摸过程中的处理
+        touchElement.on('touchmove', function (event) {
+            if ($(window).width() > demarcation && totalPages > 1) { // 只在宽屏模式生效
+                event.preventDefault(); // 防止默认的滚动行为
+                let deltaX = event.originalEvent.touches[0].clientX - startX;
+                if (deltaX > 50 && currentPage > 1) {
+                    // 向右滑动，向左翻页
+                    targetPage = currentPage - 1;
+                } else if (deltaX < -50 && currentPage < totalPages) {
+                    // 向左滑动，向右翻页
+                    targetPage = currentPage + 1;
+                }
+            }
+        });
+
+        // 触摸结束时的处理
+        touchElement.on('touchend', function (event) {
+            if (targetPage !== currentPage) {
+                $(`.page-index #${targetPage}`).click();
+            }
+        });
+    }
+
+    // 页面上下滚动时，根据滚动的距离设置 header 背景透明度
+    $(window).on('scroll', function (event) {
+        if ($(window).width() <= demarcation) { // 只在窄屏模式生效
+            const sMax = $('.header').height();
+            let st = $(window).scrollTop();
+            let sta = Math.min(st / sMax, 1);
+            $('.header').css('background-color', `rgba(50, 50, 50, ${sta})`);
+        }
+    });
+
+});
